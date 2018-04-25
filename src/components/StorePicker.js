3dfc9ef92b403c46367fa7bd692e8b418bd2f4d3
Mark <condellomark@gmail.com>
2018-04-25 19:14:22 +1000

    Added files from video 4

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,6 +2,16 @@ import React from "react";
 
 export default class StorePicker extends React.Component{
     render(){
-        return<p>Hello you piece of sexy poo.</p>
-    }
+        return (
+        <React.Fragment> 
+            <p>Fish</p>
+            <form className="store-selector">
+                <h2>Please Enter A Store</h2>
+                {/* Comments within JSX land...*/}
+                <input type="text" placeholder="Store Name"/>
+                <button type="submit">Visit Store &#8594;</button>
+            </form>
+        </React.Fragment>
+        )
+     }
 }
