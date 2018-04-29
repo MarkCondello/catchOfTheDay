e3be29ba5fb50fcff094a5f5be2bcaef3b8d3c1f
Mark <condellomark@gmail.com>
2018-04-29 10:30:03 +1000

    added video 13 files

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,31 @@ import Inventory from './Inventory';
 
 
 export default class App extends React.Component{
+    state = {
+        fishes: {},
+        order: {}
+    }
+
+    addFish = (fish) =>{
+       // console.log("Adding a fish!", fish);
+        //1. grab a copy of the current fish state
+        const fishes ={...this.state.fishes}
+         //2. add our new fish to the fishes variable with a unique id eg `fish${Date.now()}`
+        //below we are providing a unique name to newFish and adding the object fish to it.
+         fishes[`fish${Date.now()}`] = fish;
+        //3. set the newFish object to state
+        this.setState({fishes : fishes});
+    };
+
     render(){
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline = {"Fresh Seafood Market"} />
                  </div>
-                <Inventory/>
+               
                 <Order/>  
+                <Inventory addFish={this.addFish}/>
             </div>
         );
     }
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,10 +1,11 @@
 import React from "react";
+import AddFishForm from "./AddFishForm";
 
 export default class Inventory extends React.Component{
     render(){
         return(
             <div className="inventory">
-            Inventory!!!
+                <AddFishForm addFish={this.props.addFish}/>
             </div>
         );
     }
